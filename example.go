c620df1e5871f99a8e8e17267e6ec5e88b5333c6
Andrew Pryde <andrew.pryde@oracle.com>
2017-02-08 16:25:57 +0000

    bump

--- example.go
+++ example.go
@@ -12,7 +12,7 @@ import (
 const Version = "1.0.0"
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from Spinnaker - (NOPE?)!")
+	fmt.Fprintf(w, "Hello from Spinnaker - (MAYBE?)!")
 }
 
 func main() {
